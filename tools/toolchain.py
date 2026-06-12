efe7e7730b979ef00e602108deb5dfbfab279f47
Urs Stöckli <urs.stoeckli@sbb.ch>
2026-06-12 15:32:51 +0200

    Add generation of XML Snippets to toolchain.

--- tools/toolchain.py
+++ tools/toolchain.py
@@ -3,9 +3,10 @@ import os
 import shutil
 from pathlib import Path
 
-from tools.configuration import DOCS_DIR, SITE_DIR, XSD_FILE_PATH, SITE_TABLES_DIR, TEMPLATES_DIR, JEKYLL_DIR
+from tools.configuration import DOCS_DIR, SITE_DIR, XSD_FILE_PATH, SITE_TABLES_DIR, TEMPLATES_DIR, JEKYLL_DIR, SITE_XML_SNIPPETS_DIR
 from tools.expand_docs.expand_docs import expand_docs
 from tools.md_builder.md_builder import build_markdown_tables
+from tools.xml_snippets.build_xml_snippets import generate_all_snippets
 
 
 def clean(dir: str):
@@ -19,19 +20,22 @@ def copy_jekyll_files():
     dst = Path(SITE_DIR)
     shutil.copytree(src, dst, dirs_exist_ok=True)
 
-def generate_docs():
-    expand_docs(DOCS_DIR, SITE_DIR)
-    # generate_html_files(SITE_DIR)
-
 def generate_tables():
     build_markdown_tables(TEMPLATES_DIR, SITE_TABLES_DIR, XSD_FILE_PATH)
-    # generate_html_files(SITE_TABLES_DIR)
+
+def generate_xml_snippets():
+    generate_all_snippets(TEMPLATES_DIR, SITE_XML_SNIPPETS_DIR)
+
+def generate_docs():
+    expand_docs(DOCS_DIR, SITE_DIR)
 
 def main():
     clean(SITE_DIR)
     copy_jekyll_files()
-    generate_docs()
     generate_tables()
+    generate_xml_snippets()
+    generate_docs()
+
 
 if __name__ == '__main__':
     main()
